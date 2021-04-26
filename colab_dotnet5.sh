44e1648c83c8eedbffe0be6906e1a14c9fc828df
Atılım Güneş Baydin <gbaydin@users.noreply.github.com>
2021-04-26 18:04:54 +0100

    Update colab_dotnet5.sh

--- colab_dotnet5.sh
+++ colab_dotnet5.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Adds Jupyter kernels for F# and C# to a Google Colab session
 # Atilim Gunes Baydin (gunes@robots.ox.ac.uk), April 2021
-# Usage: upload an F# or C# notebook to Colab, open the notebook, then execute the following in a new cell before running anything else
+# Usage: upload an F# or C# notebook to Colab, open the notebook, then execute the following single line in a new cell before running anything else:
 # !bash <(curl -Ls https://raw.githubusercontent.com/gbaydin/scripts/main/colab_dotnet5.sh)
-echo -n "Installing dotnet-sdk-5.0 and dotnet interactive for F# and C#... "
+echo -n "Installing dotnet-sdk-5.0 and dotnet interactive for F# and C#..."
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb > /dev/null
 apt-get update > /dev/null
@@ -13,5 +13,5 @@ export PATH=$PATH:$HOME/.dotnet/tools
 dotnet interactive jupyter install > /dev/null
 echo "{\"argv\": [\"$HOME/.dotnet/tools/dotnet-interactive\", \"jupyter\", \"--default-kernel\", \"fsharp\", \"--http-port-range\", \"1000-3000\", \"{connection_file}\"], \"display_name\": \".NET (F#)\", \"language\": \"F#\"}" > /root/.local/share/jupyter/kernels/.net-fsharp/kernel.json
 echo "{\"argv\": [\"$HOME/.dotnet/tools/dotnet-interactive\", \"jupyter\", \"--default-kernel\", \"csharp\", \"--http-port-range\", \"1000-3000\", \"{connection_file}\"], \"display_name\": \".NET (C#)\", \"language\": \"C#\"}" > /root/.local/share/jupyter/kernels/.net-csharp/kernel.json
-echo "Done."
+echo " Done."
 echo "Select \"Runtime\" -> \"Change Runtime Type\" and click \"Save\" to activate for this notebook"
